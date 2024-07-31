afdae4c8e2ab221f473b3e695e21158f127ab049
Lars Siebert <github@lars-s.aleeas.com>
2024-07-31 15:35:15 +0200

    all functions implemented, sound added and responsive

--- script.js
+++ script.js
@@ -70,35 +70,88 @@ let questions = [
         "answer_3": "push()",
         "answer_4": "insert()",
         "right_answer": 3
-    }
+    },
+    {
+      "question": "Welche Methode wird verwendet, um die Anzahl der Elemente in einem Array zu ermitteln?",
+      "answer_1": "size()",
+      "answer_2": "count()",
+      "answer_3": "length()",
+      "answer_4": "length",
+      "right_answer": 4
+  }  
 ]
       
-
+let quizScore = 0;
 let currentQuestion = 0;
+let progressPercentage = 0;
+
+let audio_success = new Audio('/assets/audio/success.mp3');
+let audio_fail = new Audio('/assets/audio/fail.mp3');
+let audio_trumpets = new Audio('/assets/audio/trumpets.mp3');
 
 
-  function startQuiz() {
+  function showQuestion() {
     let mainContentRef = document.getElementById('main-content');
     mainContentRef.innerHTML = getQuizTemplate();
-
     let question = questions[currentQuestion];
+
+    document.getElementById('progress-bar').textContent = `${progressPercentage}%`
+    document.getElementById('progress-bar').style = `width: ${progressPercentage}%`
+
     document.getElementById('question-text').textContent = question['question'];                 
     document.getElementById('answer-1').textContent = question['answer_1']; 
     document.getElementById('answer-2').textContent = question['answer_2']; 
     document.getElementById('answer-3').textContent = question['answer_3']; 
     document.getElementById('answer-4').textContent = question['answer_4']; 
-    document.getElementById('questions-quantity').textContent = questions.length; 
+    document.getElementById('questions-quantity').textContent = questions.length;
+    document.getElementById('current-questions-number').textContent = currentQuestion + 1;
   }
 
 
 function answer(answerCount) {
   let question = questions[currentQuestion];
   let questionNumber = answerCount.slice(-1);
+  let rightAnswerId = `answer-${question['right_answer']}`;
   
   if (questionNumber == question['right_answer']) {
-    document.getElementById(answerCount).parentElement.classList.add('bg-success'); 
+    document.getElementById(answerCount).parentElement.classList.add('bg-success');
+    changeButtonAttributes();
+    quizScore++;
+    audio_success.play();
   } else {
-    document.getElementById(answerCount).parentElement.classList.add('bg-danger'); 
+    document.getElementById(answerCount).parentElement.classList.add('bg-danger');
+    document.getElementById(rightAnswerId).parentElement.classList.add('bg-success');
+    changeButtonAttributes();
+    audio_fail.play();
+  }
+}
+
+
+function changeButtonAttributes() {
+  document.getElementById('answer-1').parentElement.disabled = true; 
+  document.getElementById('answer-2').parentElement.disabled = true; 
+  document.getElementById('answer-3').parentElement.disabled = true; 
+  document.getElementById('answer-4').parentElement.disabled = true;
+  document.getElementById('questions-quantity-btn').disabled = false;
+}
+
+
+function nextQuestion() {
+  currentQuestion++;
+  progressPercentage = currentQuestion / questions.length;
+  progressPercentage = Math.round(progressPercentage * 100)
+  if (currentQuestion >= questions.length) {
+    document.getElementById('progress-bar').textContent = `${progressPercentage}%`
+    document.getElementById('progress-bar').style = `width: ${progressPercentage}%`
+    currentQuestion = 0;
+    progressPercentage = 0;
+
+    let mainContentRef = document.getElementById('main-content');
+    mainContentRef.innerHTML = getEndScreenTemplate();
+    quizScore = 0;
+    audio_trumpets.play();
+  } else {
+    showQuestion();
   }
 }
 
@@ -125,9 +178,20 @@ function answer(answerCount) {
     </button>
   </div>
   <div class="question-footer">
-    <span><b>1</b> von <b id="questions-quantity"></b> Fragen</span>
-    <button class="btn btn-primary">Nächste Frage</button>
+    <span><b id="current-questions-number"></b> von <b id="questions-quantity"></b> Fragen</span>
+    <button class="btn btn-primary" id="questions-quantity-btn" onclick="nextQuestion()" disabled>Nächste Frage</button>
   </div>
+</div>`
+}
+
+
+function getEndScreenTemplate() {
+  return `<div class="end-screen-content">
+  <img src="./assets/img/brain result.png" class="mb-2">
+  <h2 class="mb-5">COMPLETE QUIZ</h2>
+  <span class="mb-5" id="result-score"><b class="me-4" style="color: #F58732;">YOUR SCORE</b> <b id="quiz-score">${quizScore}</b><b>/</b><b>${questions.length}</b></span>
+  <button class="btn btn-primary mb-2">SHARE</button>
+  <button class="btn" onclick="showQuestion()">REPLAY</button>
 </div>`
 }
   
